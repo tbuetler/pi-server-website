aa9d94e29fdc758d8ac4a7695fac2b88dbcebddf
Tim Philipp Bütler <butlt2@students.bfh.ch>
2024-11-27 22:03:39 +0100

    changed ports

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+server.listen(80, () => {
+  console.log('Server running on http://localhost:80');
 });
